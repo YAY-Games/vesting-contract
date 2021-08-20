aadf804bc060d8044b6f01a20ab80cc75813a676
Алексей Жданов <zhdanoff.alexey@gmail.com>
2021-08-21 05:15:54 +0700

    feature: mercle proof scripts

--- scripts/deploy-script.js
+++ scripts/deploy-script.js
@@ -3,6 +3,7 @@ const YayVesting = artifacts.require("YayVesting");
 async function main() {
   const yayVesting = await YayVesting.new(
     process.env["TOKEN_CONTRACT"],
+    process.env["MERCLE_ROOT"],
     process.env["TGE_TIMESTAMP"],
   );
   await YayVesting.setAsDeployed(yayVesting);
