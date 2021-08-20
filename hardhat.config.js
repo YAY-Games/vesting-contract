d3397d33bc59042053243ea2ff2c0abbe9266be5
Алексей Жданов <zhdanoff.alexey@gmail.com>
2021-08-20 21:01:53 +0700

    init hardhat project

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-truffle5");
+
+const getEnv = env => {
+  const value = process.env[env];
+  if (typeof value === 'undefined') {
+    console.log(`${env} has not been set.`);
+    return "";
+  }
+  return value;
+};
+
+task("accounts", "Prints accounts", async (_, { web3 }) => {
+  console.log(await web3.eth.getAccounts());
+});
+
+module.exports = {
+  // defaultNetwork: "hardhat",
+  // networks: {
+  //   hardhat: {
+  //   },
+  //   // bsc: {
+  //   //   url: getEnv('RPC_URL'),
+  //   //   accounts: [getEnv('PRIVATE_KEY')]
+  //   // }
+  // },
+  solidity: {
+    version: "0.6.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+};
