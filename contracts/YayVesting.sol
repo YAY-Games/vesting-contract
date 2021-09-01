d40f4d557a3ee9e1d1f5f3aeda93077a2c5af9ab
Алексей Жданов <zhdanoff.alexey@gmail.com>
2021-09-01 20:48:56 +0700

    update: new tokenomics

--- contracts/YayVesting.sol
+++ contracts/YayVesting.sol
@@ -66,7 +66,7 @@ contract YayVesting {
             totalSteps: 15,
             stepTime: 30 days,
             percentBefore: 10_00,
-            percentAfter: 6_00  // 6.00% * 15 + 10.00% = 100.00%
+            percentAfter: 6_00  // 10.00% + 6.00% * 15 = 100.00%
         });
         categories[CategoryNames.STRATEGIC] = CategoryType({
             totalSteps: 12,
@@ -75,16 +75,16 @@ contract YayVesting {
             percentAfter: 7_50  // 10.00% + 7.50% * 12 = 100.00%
         });
         categories[CategoryNames.PRESALE] = CategoryType({
-            totalSteps: 5,
+            totalSteps: 4,
             stepTime: 30 days,
-            percentBefore: 30_00,
-            percentAfter: 14_00  // 30.00% + 14.00% * 5 = 100.00%
+            percentBefore: 10_00,
+            percentAfter: 22_50  // 10.00% + 22.50% * 4 = 100.00%
         });
         categories[CategoryNames.PUBLIC] = CategoryType({
             totalSteps: 8,
             stepTime: 7 days,
-            percentBefore: 30_00,
-            percentAfter: 8_75  // 30.00% + 8.75% * 8 = 100.00%
+            percentBefore: 20_00,
+            percentAfter: 10_00  // 20.00% + 10.00% * 8 = 100.00%
         });
         categories[CategoryNames.V24MONTH] = CategoryType({
             totalSteps: 23,
